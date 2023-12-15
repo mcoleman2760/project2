68f96effb9315fc9001177581b9b62340ec0f608
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-15 17:37:52 -0500

    Update MergeSort.hpp

--- MergeSort.hpp
+++ MergeSort.hpp
@@ -7,22 +7,23 @@
 int mergeSort(std::vector<int>& nums, int& duration) {
     auto start = std::chrono::high_resolution_clock::now();
 
-    int median = 0;
+    std::vector<int> aux(nums.size());
 
-    if (nums.size() > 1) {
-        auto middle = nums.begin() + nums.size() / 2;
+    for (int width = 1; width < nums.size(); width *= 2) {
+        for (int i = 0; i < nums.size(); i += 2 * width) {
+            int left = i;
+            int middle = std::min(i + width, static_cast<int>(nums.size()));
+            int right = std::min(i + 2 * width, static_cast<int>(nums.size()));
 
-        int medianLeft = mergeSort(std::vector<int>(nums.begin(), middle), duration);
-        int medianRight = mergeSort(std::vector<int>(middle, nums.end()), duration);
+            std::merge(nums.begin() + left, nums.begin() + middle,
+                       nums.begin() + middle, nums.begin() + right,
+                       aux.begin() + left);
+        }
 
-        std::vector<int> merged;
-        std::merge(nums.begin(), middle, middle, nums.end(), std::back_inserter(merged));
-        std::copy(merged.begin(), merged.end(), nums.begin());
-
-        median = (medianLeft + medianRight) / 2;
-    } else if (!nums.empty()) {
-        median = nums[0];
+        std::copy(aux.begin(), aux.end(), nums.begin());
     }
+
+    int median = nums[nums.size() / 2];
 
     auto stop = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
