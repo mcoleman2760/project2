e0954256fead6f8767865a9834d799b90bbbfcb8
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-15 15:34:37 -0500

    Create StandardSort.hpp

---
+++ StandardSort.hpp
@@ -0,0 +1,19 @@
+#ifndef STANDARD_SORT_HPP
+#define STANDARD_SORT_HPP
+
+#include <vector>
+#include <algorithm>
+#include <chrono>
+
+int standardSort(std::vector<int>& nums, int& duration) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::sort(nums.begin(), nums.end());
+
+    auto end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    return nums[nums.size() / 2];
+}
+
+#endif  
