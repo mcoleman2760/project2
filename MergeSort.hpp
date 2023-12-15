7c4ce0603ba623d6bae1399ed7c4b5769d01a44f
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-15 17:28:36 -0500

    Update MergeSort.hpp

--- MergeSort.hpp
+++ MergeSort.hpp
@@ -1,64 +1,31 @@
-#ifndef MERGE_SORT_HPP
-#define MERGE_SORT_HPP
-
+#include <iostream>
 #include <vector>
+#include <iterator>
 #include <algorithm>
-#include <chrono>
+#include <chrono>  // for std::merge
 
-template <typename Iterator>
-void merge(Iterator begin, Iterator mid, Iterator end) {
-    using T = typename std::iterator_traits<Iterator>::value_type;
-    std::vector<T> merged(std::distance(begin, end));
-
-    auto left = begin;
-    auto right = mid;
-    auto mergedIt = merged.begin();
-
-    while (left < mid && right < end) {
-        if (*left < *right) {
-            *mergedIt++ = *left++;
-        } else {
-            *mergedIt++ = *right++;
-        }
-    }
-
-    // Copy the remaining elements from the left and right subarrays
-    std::copy(left, mid, mergedIt);
-    std::copy(right, end, mergedIt);
-
-    // Copy the merged result back to the original range
-    std::copy(merged.begin(), merged.end(), begin);
-}
-
-template <typename Iterator>
-int mergeSort(Iterator begin, Iterator end, int& duration) {
+int mergeSort(std::vector<int>& nums, int& duration) {
     auto start = std::chrono::high_resolution_clock::now();
 
-    size_t size = std::distance(begin, end);
+    int median = 0;
 
-    if (size <= 1) {
-        auto stop = std::chrono::high_resolution_clock::now();
-        duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
-        return *begin;  // Base case: already sorted
+    if (nums.size() > 1) {
+        auto middle = nums.begin() + nums.size() / 2;
+
+        int medianLeft = mergeSort(std::vector<int>(nums.begin(), middle), duration);
+        int medianRight = mergeSort(std::vector<int>(middle, nums.end()), duration);
+
+        std::vector<int> merged;
+        std::merge(nums.begin(), middle, middle, nums.end(), std::back_inserter(merged));
+        std::copy(merged.begin(), merged.end(), nums.begin());
+
+        median = (medianLeft + medianRight) / 2;
+    } else if (!nums.empty()) {
+        median = nums[0];
     }
-
-    Iterator mid = std::next(begin, size / 2);
-
-    // Recursively sort the left and right halves
-    int leftMedian = mergeSort(begin, mid, duration);
-    int rightMedian = mergeSort(mid, end, duration);
-
-    // Merge the sorted halves
-    merge(begin, mid, end);
 
     auto stop = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
 
-    return (*begin + *(mid - 1)) / 2;  // Return the median of the merged sorted halves
+    return median;
 }
-
-int mergeSort(std::vector<int>& nums, int& duration) {
-    return mergeSort(nums.begin(), nums.end(), duration);
-}
-
-#endif  // MERGE_SORT_HPP
