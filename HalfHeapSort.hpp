a0ee7a6d8e75850c2fe259f0dff8a6fb26dbb7dd
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-15 18:30:28 -0500

    Update HalfHeapSort.hpp

--- HalfHeapSort.hpp
+++ HalfHeapSort.hpp
@@ -12,8 +12,8 @@ void percDown(std::vector<int>& heap, std::vector<int>::size_type hole){
         std::vector<int>::size_type leftChild = 2 * hole + 1;
         std::vector<int>::size_type rightChild = leftChild + 1;
 
-        // If the right child exists and is greater than the left child
-        if (rightChild < heap.size() - 1 && heap[rightChild] > heap[leftChild]) {
+        // If the right child exists and is greater than or equal to the left child
+        if (rightChild < heap.size() - 1 && heap[rightChild] >= heap[leftChild]) {
             ++leftChild; // Move to the left child
         }
 
@@ -42,17 +42,22 @@ int halfHeapSort(std::vector<int>& nums, int& duration){
     buildHeap(nums);
 
     // Perform heap sort
-    for (std::vector<int>::size_type i = nums.size() / 2; i < nums.size(); ++i) {
+    for (std::vector<int>::size_type i = nums.size() / 2; i > 0; i--) {
         std::swap(nums[0], nums[i]);
-        percDown(nums, i);
+        percDown(nums, nums.size());
     }
 
-    int median = nums[nums.size() / 2];
+  //  int median = nums[nums.size() / 2];
 
     auto stop = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+    if (nums.size() % 2 == 0){
+      return nums[((nums.size() / 2) +((nums.size() / 2)-1)) /2 ];
 
-    return median;
+    }
+    return nums[nums.size() / 2];
+
+    
 }
 
 
