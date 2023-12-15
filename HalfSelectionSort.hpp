fa89d1fc9faa1a74cbcda994a772feadf1b86a7e
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-15 16:22:54 -0500

    Update HalfSelectionSort.hpp

--- HalfSelectionSort.hpp
+++ HalfSelectionSort.hpp
@@ -1,47 +1,50 @@
+
 #ifndef HALF_SELECTION_SORT_HPP
 #define HALF_SELECTION_SORT_HPP
 
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iterator>
 #include <chrono>
-
-template <typename Iterator>
-Iterator findSmallest(Iterator begin, Iterator end) {
-    Iterator smallest = begin;
-    for (Iterator it = begin + 1; it != end; ++it) {
-        if (*it < *smallest || (*it == *smallest && it < smallest)) {
-            smallest = it;
-        }
-    }
-    return smallest;
-}
-
-template <typename Iterator>
-void swapSmallestToPosition(Iterator begin, Iterator end, size_t position) {
-    Iterator smallest = findSmallest(begin, end);
-    std::iter_swap(begin + position, smallest);
-}
+#include <iostream>
 
 int halfSelectionSort(std::vector<int>& nums, int& duration) {
     auto start = std::chrono::high_resolution_clock::now();
 
-    if (nums.size() > 50000) {
-        std::cout << "Input is too big for half selection sort." << std::endl;
-        return 0;  // Placeholder value for the median
+    size_t size = nums.size();
+    size_t halfSize = size / 2;
+
+    // Check if the input size is too big for selection sort
+    if (size > 50000) {
+        std::cout << "Input size is too big for selection sort (>50,000)." << std::endl;
+        duration = -1;  // Indicate that the method was not executed due to input size
+        return -1;  // Return an invalid value
     }
 
-    size_t targetIndex = nums.size() / 2;
+    for (size_t i = 0; i < halfSize; ++i) {
+        // Find the index of the minimum element in the remaining unsorted part
+        auto minIndex = std::min_element(nums.begin() + i, nums.end());
 
-    for (auto it = nums.begin(); it != nums.begin() + targetIndex; ++it) {
-        swapSmallestToPosition(it, nums.end(), std::distance(nums.begin(), it));
+        // Swap the current element with the smallest element found
+        std::iter_swap(nums.begin() + i, minIndex);
+
+        // If there are duplicates of the smallest element, swap the first one reached
+        auto firstDuplicate = std::find(nums.begin() + i + 1, nums.end(), nums[i]);
+        if (firstDuplicate != nums.end()) {
+            std::iter_swap(nums.begin() + i + 1, firstDuplicate);
+        }
     }
 
-    auto end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    // Swap the median element into its proper place
+    size_t medianIndex = size / 2;
+    auto minIndex = std::min_element(nums.begin() + medianIndex, nums.end());
+    std::iter_swap(nums.begin() + medianIndex, minIndex);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
 
     // Return the median
-    return nums[targetIndex];
+    return nums[medianIndex];
 }
 
 #endif  // HALF_SELECTION_SORT_HPP
