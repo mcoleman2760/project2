9998588cfe46cb4cea32b7e88e5714fb86c2ee5f
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-16 23:51:11 -0500

    Update QuickSelect.hpp

--- QuickSelect.hpp
+++ QuickSelect.hpp
@@ -8,27 +8,34 @@
 std::vector<int>::iterator medianOfThree(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high) {
     std::vector<int>::iterator middle = low + (high - low) / 2;
 
-    // Sorts the low, middle, and high elements of the vector
-    if (*middle < *low) {
-        std::iter_swap(middle, low);
+    // Identify the median of low, middle, and high elements
+    std::vector<int>::iterator median;
+    if (*middle < *low && *low < *high) {
+        median = low;
+    } else if (*high < *low && *low < *middle) {
+        median = low;
+    } else if (*low < *middle && *middle < *high) {
+        median = middle;
+    } else if (*high < *middle && *middle < *low) {
+        median = middle;
+    } else {
+        median = high;
     }
-    if (*high < *low) {
-        std::iter_swap(high, low);
-    }
-    if (*high < *middle) {
-        std::iter_swap(high, middle);
-    }
-    // returns median
-    return middle;
+
+    // Swap the median into the "high" index
+    std::iter_swap(median, high);
+
+    return high;
 }
 
 // Function to perform Hoare partition with median-of-three pivot selection and placement
 std::vector<int>::iterator hoarePartition(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high) {
-    std::vector<int>::iterator pivot = medianOfThree(nums, low, high);
-
+    // Pivot is assumed to be in the correct position already (in this case, at 'high')
+    std::vector<int>::iterator pivot = high;
     int pivotValue = *pivot;
+
     auto i = low;
-    auto j = high;
+    auto j = high - 1; // Start 'j' one position before the pivot
 
     while (true) {
         // Move i to the right until an element greater than or equal to the pivot is found
@@ -42,7 +49,7 @@ std::vector<int>::iterator hoarePartition(std::vector<int>& nums, std::vector<in
         }
 
         if (i >= j) {
-            return j;
+            return i; // 'i' is the correct position for the pivot
         }
 
         std::iter_swap(i, j);
@@ -55,6 +62,7 @@ std::vector<int>::iterator hoarePartition(std::vector<int>& nums, std::vector<in
 
 // Function to perform quick select with median-of-three pivot selection and placement
 int quickSelect(std::vector<int>& nums, int& duration) {
+    // start of clock for duration
     auto start = std::chrono::high_resolution_clock::now();
 
     // Using Hoare's partition with median-of-three pivot selection and placement
