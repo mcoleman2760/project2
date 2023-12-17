4e0cfe35b8540bd42493ce248becea304cad634e
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-16 23:29:29 -0500

    Update StandardSort.hpp

--- StandardSort.hpp
+++ StandardSort.hpp
@@ -6,13 +6,14 @@
 #include <chrono>
 
 int standardSort(std::vector<int>& nums, int& duration) {
+  // starts the clock to find the duration
     auto start = std::chrono::high_resolution_clock::now();
-
+    // use std::sort function to sort the vector
     std::sort(nums.begin(), nums.end());
-
+    //end of clock for duration
     auto end = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    
+    // finds the median for even and odd numbered vectors
     if (nums.size() % 2 == 0){
       return nums[((nums.size() / 2) +((nums.size() / 2)-1)) /2 ];
 
