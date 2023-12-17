67b27358a0a5ecd3a1fa00971eb6cc193a9b78d6
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-16 23:28:42 -0500

    Update InPlaceMergeSort.hpp

--- InPlaceMergeSort.hpp
+++ InPlaceMergeSort.hpp
@@ -5,26 +5,27 @@
 #include <chrono>
 
 int inPlaceMergeSort(std::vector<int>& nums, int& duration) {
+    // start of clock for duration
     auto start = std::chrono::high_resolution_clock::now();
 
     std::vector<int> aux(nums.size());
-
+    //iterates through the vector
     for (int width = 1; width < nums.size(); width *= 2) {
         for (int i = 0; i < nums.size(); i += 2 * width) {
             int left = i;
             int middle = std::min(i + width, static_cast<int>(nums.size()));
             int right = std::min(i + 2 * width, static_cast<int>(nums.size()));
-
+            // uses std::inplace_merge instead of std::merge to merge the different sorted
             std::inplace_merge(nums.begin() + left, nums.begin() + middle, nums.begin() + right);
         }
     }
 
-    //int median = nums[nums.size() / 2];
-
+   
+    //ends the clock for duration
     auto stop = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
 
-
+    // finds the median
     if (nums.size() % 2 == 0){
       return nums[((nums.size() / 2) +((nums.size() / 2)-1)) /2 ];
     }
