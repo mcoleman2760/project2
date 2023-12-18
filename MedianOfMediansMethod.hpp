4e8c3b1d63f0eb8a0044bc53544cf17edff70608
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-17 21:32:37 -0500

    Update MedianOfMediansMethod.hpp

--- MedianOfMediansMethod.hpp
+++ MedianOfMediansMethod.hpp
@@ -5,36 +5,47 @@
 #include <chrono>
 #include "QuickSelect.hpp"
 
-int findMedianOfFive(std::vector<int>::iterator low, std::vector<int>::iterator high) {
+// calculates the median of 5 numbers in 6 comparisons
+int medianOfFive(std::vector<int>::iterator low, std::vector<int>::iterator high) {
     std::sort(low, high);
     return *(low + (high - low - 1) / 2);
 }
 
-
-// Recursive function to find the median of medians
+// Recursive function for the median of medians
 int medianOfMedians(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high) {
     int n = high - low;
     if (n <= 5) {
-        // If the number of elements is small, just sort and find the median
+        // If the number of elements is small, sort them and find the median
         std::sort(low, high);
         return *(low + n / 2);
     }
 
-    // Find the median of each group of 5 elements
-    std::vector<int>::iterator groupMedian = hoarePartition(nums, low, high);
+    // Find the median of the different groups of 5 elements using Hoare's partition
+    std::vector<int>::iterator groupMedian = low;
+    for (std::vector<int>::iterator i = low; i < high; i += 5) {
+        std::vector<int>::iterator groupHigh = std::min(i + 5, high);
+        int median = medianOfFive(i, groupHigh);
+        std::swap(*std::find(i, groupHigh, median), *groupMedian);
+        ++groupMedian;
+    }
 
-    // Recursively find the median of medians
-    return medianOfMedians(nums, low, groupMedian + 1);
+    // Use hoarePartition from quickselect function to partition since this algorithm is a pivot selection method
+    std::vector<int>::iterator pivot = hoarePartition(nums, low, groupMedian);
+
+    // uses recursion to find the medians of medians
+    return medianOfMedians(nums, low, pivot + 1);
 }
 
-// Wrapper function to call the median of medians algorithm
+// A wrapper function to call the median of medians algorithm
 int medianOfMediansMethod(std::vector<int>& nums, int& duration) {
+    // start of clock for duration time
     auto start = std::chrono::high_resolution_clock::now();
 
     int result = medianOfMedians(nums, nums.begin(), nums.end());
 
+    // give the duration time
     auto end = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-
+    // returns the median
     return result;
 }
